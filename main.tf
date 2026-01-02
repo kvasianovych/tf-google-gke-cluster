1a3a92d75a925b1c12bfd103bd8b83f6cd8d6dfc
Kostiantyn Vasianovych <k.vasianovych@pm.me>
2026-01-02 20:16:57 +0200

    Update Terraform code

--- main.tf
+++ main.tf
@@ -3,19 +3,15 @@ provider "google" {
   # The GCP project to use
   project = var.GOOGLE_PROJECT
   # The GCP region to deploy resources in
-  region  = var.GOOGLE_REGION
+  region = var.GOOGLE_REGION
 }
 
 # Create the GKE (Google Kubernetes Engine) cluster
 resource "google_container_cluster" "this" {
-  # Name of the cluster
   name     = var.GKE_CLUSTER_NAME
-  # Location (region) for the cluster
   location = var.GOOGLE_REGION
 
-  # Set initial node count (required, but will remove default pool)
   initial_node_count       = 1
-  # Remove default node pool to use custom node pools instead
   remove_default_node_pool = true
 
   # Workload Identity configuration for GKE
@@ -33,36 +29,30 @@ resource "google_container_cluster" "this" {
 
 # Create a custom node pool for the GKE cluster
 resource "google_container_node_pool" "this" {
-  # Name of the node pool
-  name       = var.GKE_POOL_NAME
-  # GCP project to use (derived from the cluster)
-  project    = google_container_cluster.this.project
-  # Attach node pool to the created cluster
   cluster    = google_container_cluster.this.name
-  # Location (region)
   location   = google_container_cluster.this.location
-  # Number of nodes in the pool
+  name       = var.GKE_POOL_NAME
   node_count = var.GKE_NUM_NODES
+  project    = google_container_cluster.this.project
 
   # Node configuration
   node_config {
-    # Machine type for the nodes
     machine_type = var.GKE_MACHINE_TYPE
   }
 }
 
 # Module to authenticate with GKE cluster using native Terraform module
 module "gke_auth" {
+  source  = "terraform-google-modules/kubernetes-engine/google//modules/auth"
+  version = ">= 42.0.0"
+
+  cluster_name = google_container_cluster.this.name
+  location     = var.GOOGLE_REGION
+  project_id   = var.GOOGLE_PROJECT
+
   depends_on = [
     google_container_cluster.this
   ]
-  # Source of the module (Terraform Registry)
-  source       = "terraform-google-modules/kubernetes-engine/google//modules/auth"
-  version      = ">= 24.0.0"
-  # Project and cluster details for authentication
-  project_id   = var.GOOGLE_PROJECT
-  cluster_name = google_container_cluster.this.name
-  location     = var.GOOGLE_REGION
 }
 
 # Data source to retrieve the current Google client configuration
@@ -70,8 +60,6 @@ data "google_client_config" "current" {}
 
 # Data source to fetch details about the created GKE cluster
 data "google_container_cluster" "main" {
-  # Name of the cluster
   name     = google_container_cluster.this.name
-  # Location (region)
   location = var.GOOGLE_REGION
 }
